44eb8ff963f876a38c117328d3d098621fccfb6c
Will Westrop <wwestrop@hotmail.com>
2022-06-29 19:11:31 +0100

    Correct GUIDs, bump version

--- build.js
+++ build.js
@@ -5,11 +5,11 @@ import webExt from "web-ext";
 
 const extensions = [
     { name: "Overview", icon: "overview.png", urlSuffix: "", guid: "{6955d06d-99e4-41bf-add9-14bcdb99ae1b}" },
-    { name: "Work Items", icon: "plan.png", urlSuffix: "_workitems", guid: "{a49290b8-292e-41c2-976d-c75b2a3c59d3}" },
-    { name: "Git", icon: "repos.png", urlSuffix: "_git", guid: "{fb28f589-8cad-424e-aa48-962ed6691574}" },
+    { name: "Work Items", icon: "plan.png", urlSuffix: "_workitems", guid: "{304a4a6a-8af1-4db3-9483-afabcd1aef5b}" },
+    { name: "Git", icon: "repos.png", urlSuffix: "_git", guid: "{a7db9a67-7e24-40f3-88ad-6117febfaba1}" },
     { name: "Pipelines", icon: "pipelines.png", urlSuffix: "_build", guid: "{91963394-6b51-43f2-9e29-b48b1716f548}" },
     { name: "Test Plans", icon: "tests.png", urlSuffix: "_testPlans", guid: "{44f9faff-afc1-4bbd-a194-cf8edee09e65}" },
-    { name: "Artifacts", icon: "artifacts.png", urlSuffix: "_artifacts", guid: "{fd6eebf6-5f7e-44b6-a732-b8405e4f7d66}" }
+    { name: "Artifacts", icon: "artifacts.png", urlSuffix: "_artifacts", guid: "{a514e12a-c40d-4549-9541-79e72f0b3226}" }
 ];
 
 const version = JSON.parse(fs.readFileSync("package.json", { encoding: "utf8" })).version;
